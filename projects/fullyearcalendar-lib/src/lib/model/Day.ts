301c367143655610b7eff6d12175c1afd97aa2a1
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2021-08-06 14:20:00 +0200

    lint: format Day class

--- projects/fullyearcalendar-lib/src/lib/model/Day.ts
+++ projects/fullyearcalendar-lib/src/lib/model/Day.ts
@@ -1,11 +1,11 @@
 import {Range} from './Range';
 
 export class Day {
-    day:Date;
-    dayOfWeek:string;
-    init:boolean = false;
-    color:string;
-    tooltip:string;
-    ranges:Range[];
-    isDisabled?:boolean = false;
+    day: Date;
+    dayOfWeek: string;
+    init = false;
+    color: string;
+    tooltip: string;
+    ranges: Range[];
+    isDisabled = false;
 }
