cba71c97e8d1ecda529df8fc345ca3a054aaf0bd
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2021-08-09 13:21:04 +0200

    feat: update version

--- projects/fullyearcalendar-lib/src/lib/specs/model.spec.ts
+++ projects/fullyearcalendar-lib/src/lib/specs/model.spec.ts
@@ -4,21 +4,17 @@ import { Month } from '../model/Month';
 describe('Year model Test', () => {
 
   it('months should have 12 months', () => {
-    expect(new Year(2019).months.length).toBe(12);
+    expect(new Year(2019, 0, 0).months.length).toBe(12);
   });
 
-  it('year should by 2018',()=> {
-      expect(new Year(2018).year).toBe(2018);
+  it('year should by 2018', () => {
+      expect(new Year(2018, 0, 0).year).toBe(2018);
   });
 
-  it('days of a month',() => {
-    expect(new Year(2019).getMonthDays(1).length).toBe(28);
-    expect(new Year(2019).getMonthDays(2).length).toBe(31);
-    expect(new Year(2019).getMonthDays(3)[0].day.getDay()).toBe(1)
-  });
-
-  it('month should have weeks',() => {
-    expect(new Month(new Year(2017).getMonthDays(3), this.startingDay).weeks.length).toBe(6);
+  it('days of a month', () => {
+    expect(new Year(2019, 0, 0).getMonthDays(1, 2019).length).toBe(28);
+    expect(new Year(2019, 0, 0).getMonthDays(2, 2019).length).toBe(31);
+    expect(new Year(2019, 0, 0).getMonthDays(3, 2019)[0].day.getDay()).toBe(1);
   });
 
 });
